208ea92f978ded9b512823440e81bd87dbc12a35
Eduardo Ribas <Edward@Mac-mini-de-Ente.local>
2022-04-08 17:07:56 -0300

    Some updates and ready article.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h3>Blog simples</h3>
-<br>
-Responsável: Eduardo Ribas<br>
-Tema: Web Development (Front-End)<br>
-Para acessar o projeto, clique <a href="https://edwardribas.github.io/gcw-blog/">aqui</a>.
+
+Responsável: <b>Eduardo Ribas</b><br>
+Tema: Desenvolvimento Web<br>
+Caso queira acessar, clique <a href="https://edwardribas.github.io/gcw-blog/">aqui</a>.
--- assets/js/articles.js
+++ assets/js/articles.js
@@ -1,14 +1,16 @@
 const specialHeader = document.querySelector("header.hidden"),
 specialNav = document.querySelector('header.hidden nav'),
-menuSpecialBtn = document.querySelector('header.hidden .menu-toggler')
+menuSpecialBtn = document.querySelector('header.hidden .menu-toggler'),
+normalNav = document.querySelector('header:not(.hidden) nav')
 ;
 
 const toggleHeader = () => {
-    if(scrollY > 100){
+    if(scrollY > 120){
         specialHeader.classList.add("active");
+        normalNav.classList.remove('active');
     }else{
         specialHeader.classList.remove("active");
-
+        specialNav.classList.remove('active');
     }
 }
 window.onscroll = () => toggleHeader();
