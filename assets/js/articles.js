308611eeeed46c89a7582979339a8e8e22a26484
Eduardo Ribas <ribasedu2005@gmail.com>
2022-06-06 11:01:50 -0300

    Tags keywords addition and some general improvements.

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -1,18 +1,25 @@
 const specialHeader = document.querySelector("header.hidden"),
-specialNav = document.querySelector('header.hidden nav'),
-menuSpecialBtn = document.querySelector('header.hidden .menu-toggler'),
-normalNav = document.querySelector('header:not(.hidden) nav')
+    specialNav = document.querySelector('header.hidden nav'),
+    menuSpecialBtn = document.querySelector('header.hidden .menu-toggler'),
+    normalNav = document.querySelector('header:not(.hidden) nav'),
+    metaTags = document.querySelector('#tags').getAttribute('content').split(", "),
+    tagsWrapper = document.querySelector('.tags')
 ;
 
 const toggleHeader = () => {
-    if(scrollY > 120){
-        specialHeader.classList.add("active");
-        normalNav.classList.remove('active');
-    }else{
-        specialHeader.classList.remove("active");
-        specialNav.classList.remove('active');
-    }
+    specialHeader.classList.toggle('active', scrollY > 120);
+    normalNav.classList.toggle('active', scrollY > 120);
 }
+
 window.onscroll = () => toggleHeader();
 window.onload = () => toggleHeader();
+
 menuSpecialBtn.onclick = () => specialNav.classList.toggle("active");
+
+window.onload = () => {
+    for(let tag of metaTags) {
+        let span = document.createElement('span');
+        span.textContent = tag;
+        tagsWrapper.appendChild(span)
+    }
+}
