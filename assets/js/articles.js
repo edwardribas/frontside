8e53c7a393d14fba9f86bd5b3d97c7f7f508452c
Eduardo Ribas <ribasedu2005@gmail.com>
2022-06-06 15:39:55 -0300

    Additions and improvements.

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -1,25 +1,33 @@
 const specialHeader = document.querySelector("header.hidden"),
     specialNav = document.querySelector('header.hidden nav'),
     menuSpecialBtn = document.querySelector('header.hidden .menu-toggler'),
-    normalNav = document.querySelector('header:not(.hidden) nav'),
-    metaTags = document.querySelector('#tags').getAttribute('content').split(", "),
-    tagsWrapper = document.querySelector('.tags')
+    normalNav = document.querySelector('header:not(.hidden) nav')
 ;
 
 const toggleHeader = () => {
     specialHeader.classList.toggle('active', scrollY > 120);
-    normalNav.classList.toggle('active', scrollY > 120);
+    if (normalNav.classList.contains('active') && scrollY > 120 )
+        normalNav.classList.remove('active');
+        specialNav.classList.remove('active');
 }
 
 window.onscroll = () => toggleHeader();
 window.onload = () => toggleHeader();
 
+// Open Menu
 menuSpecialBtn.onclick = () => specialNav.classList.toggle("active");
 
-window.onload = () => {
+// Load Page Tags
+if (document.querySelector('#tags')) {
+    let metaTags = document.querySelector('#tags'); // get the content of keywords meta
+    metaTags = metaTags.getAttribute('content').split(", "); // get array from string
+    tagsWrapper = document.querySelector('.tags'); // calls the tag wrapper
+
+    // iterate above the array
     for(let tag of metaTags) {
         let span = document.createElement('span');
         span.textContent = tag;
+        span.onclick = () => scrollTo(0, 0)
         tagsWrapper.appendChild(span)
     }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
-const upButton = document.querySelector('.up-btn');
+const upButton = document.querySelector('.up-btn'),
+      shuffleButton = document.querySelector('.text button'),
+      articlesLinks = document.querySelectorAll('article > a')
+;
 
 const toggleUpBtn = () => {
     if(scrollY > 300){
@@ -10,8 +13,18 @@ const toggleUpBtn = () => {
 
 window.onscroll = () => {
     toggleUpBtn();
-    if(window.scrollY >= 100){
-        nav.classList.remove('active')
-    }
+    if (window.scrollY >= 100) nav.classList.remove('active')
 }
 window.onload = () => toggleUpBtn()
+
+// Get Random Link
+shuffleButton.onclick = () => {
+    let links = [];
+    for (let link of articlesLinks) {
+        links.push(link.getAttribute('href'));
+    }
+
+    let randomNumber = Math.floor(Math.random() * links.length);
+
+    location.href = links[randomNumber];
+}
